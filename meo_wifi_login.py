6d141808ef17267fa9c9c8c35e7ceac4d9923d3b
João Ricardo Sares Teles de Matos <joao.r.matos@ist.utl.pt>
2019-09-09 12:11:09 +0100

    Use docstrings + fix indentation

--- meo_wifi_login.py
+++ meo_wifi_login.py
@@ -67,63 +67,68 @@ def encrypt_cryptography(key, iv, msg):
   encryptor = cipher.encryptor()
   return encryptor.update(msg_padded) + encryptor.finalize()
 
-# Encrypt msg using AES in CBC mode and PKCS#7 padding, using either
-# cryptography or pyaes
 def encrypt(key, iv, msg):
+  """Encrypt msg using AES in CBC mode and PKCS#7 padding.
+
+  Will use either the cryptography or pyaes module, whichever is
+  available.
+
+  """
   if "cryptography" in sys.modules:
     return encrypt_cryptography(key, iv, msg)
   elif "pyaes" in sys.modules:
     return encrypt_pyaes(key, iv, msg)
 
-# Encrypt the password like the captive portal's Javascript does
 def encrypt_password(ip, password):
-    # Salt for PBKDF2
-    salt = bytearray.fromhex("77232469666931323429396D656F3938574946")
-    # Initialization vector for CBC
-    iv = bytes(bytearray.fromhex("72c4721ae01ae0e8e84bd64ad66060c4"))
-    
-    # Generate key from IP address
-    key = hashlib.pbkdf2_hmac("sha1", ip.encode("utf8"), salt, 100, dklen=32)
-    
-    # Encrypt password
-    ciphertext = encrypt(key, iv, password)
-    
-    # Encode to Base64 (explicitly convert to string for Python 2/3 compat)
-    ciphertext_b64 = base64.b64encode(ciphertext).decode("ascii")
-    
-    return ciphertext_b64
+  """Encrypt the password like the captive portal's Javascript does"""
+  # Salt for PBKDF2
+  salt = bytearray.fromhex("77232469666931323429396D656F3938574946")
+  # Initialization vector for CBC
+  iv = bytes(bytearray.fromhex("72c4721ae01ae0e8e84bd64ad66060c4"))
+  
+  # Generate key from IP address
+  key = hashlib.pbkdf2_hmac("sha1", ip.encode("utf8"), salt, 100, dklen=32)
+  
+  # Encrypt password
+  ciphertext = encrypt(key, iv, password)
+  
+  # Encode to Base64 (explicitly convert to string for Python 2/3 compat)
+  ciphertext_b64 = base64.b64encode(ciphertext).decode("ascii")
+  
+  return ciphertext_b64
 
 ### Misc
 
-# Reads a line from standard input (Python 2/3 compat)
 def get_input(prompt):
+  """Reads a line from standard input (Python 2/3 compat)"""
   if sys.version_info < (3, 0):
     return raw_input(prompt)
   else:
     return input(prompt)
 
-# Read a jsonp string.
 def read_jsonp(jsonp):
+  """Read a jsonp string."""
   jsonp_stripped = jsonp[ jsonp.index("(")+1 : jsonp.rindex(")") ]
   return json.loads(jsonp_stripped)
 
 ### Main application logic
 
-# Get the state of the connection from the server
 def get_state():
+  """Get the state of the connection from the server"""
   url = 'https://servicoswifi.apps.meo.pt/HotspotConnection.asmx/GetState?callback=foo&mobile=false&pagePath=foo'
   response = requests.get(url)
   state = read_jsonp(response.content.decode(response.encoding))
   return state
 
-# Get our LAN IP address
 def get_ip():
-  # It's included in the connection state. That's where the web interface gets it, too.
+  """Get our LAN IP address"""
+  # It's included in the connection state.
+  # That's where the web interface gets it, too.
   state = get_state()
   return state["FrammedIp"]
 
-# Make a GET request with the required data to login to a MEO Wifi Premium Hotspot
 def meo_wifi_login(username, password):
+  """Make a GET request with the required data to login to a MEO Wifi Premium Hotspot"""
   ip = get_ip()
   encrypted_password = encrypt_password(ip, password)
   url ='https://servicoswifi.apps.meo.pt/HotspotConnection.asmx/Login?username=' + username+ '&password=' + encrypted_password + '&navigatorLang=pt&callback=foo'
@@ -131,8 +136,8 @@ def meo_wifi_login(username, password):
 
   return response
 
-# Make a GET request to logoff from a MEO Wifi Premium Hotspot
 def meo_wifi_logoff():
+  """Make a GET request to logoff from a MEO Wifi Premium Hotspot"""
   url = 'https://servicoswifi.apps.meo.pt/HotspotConnection.asmx/Logoff?callback=foo'
   response = requests.get(url)
 
